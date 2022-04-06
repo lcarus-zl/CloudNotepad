ed95172dc6730d058420586f298bf1937fd71eb9
lcarus-zl <zl351889@163.com>
2022-04-06 23:56:56 +0800

    http-server正常

--- build/mock.config.js
+++ build/mock.config.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-const mockBaseURL = 'http://note-server.hunger-valley.com'
-const realBaseURL = 'http://note-server.hunger-valley.com'
+const mockBaseURL = 'https://note-server.hunger-valley.com'
+const realBaseURL = 'https://note-server.hunger-valley.com'
 
 exports.config = function({ isDev = true } = { isDev: true} ) {
   let fileTxt = `
